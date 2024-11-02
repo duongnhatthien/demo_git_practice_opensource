a9d5800ce01c28a6b9a6ab41b1f6996d5404828e
duy tung <duytung9903@gmail.com>
2024-11-02 11:14:40 +0700

    define sub method

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a,b;
+	cin >>a>>b;
+	int x =hieu(a,b);
+	cout <<"hieu la: "<< x ;
 	system("pause");
 	return 0;
 }
